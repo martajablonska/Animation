b0060270bab7fb730211dbe43a53507d66d2950c
martajablonska <martawaldon@gmail.com>
2018-12-18 12:33:54 +0100

    Added basic style and photos

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,66 @@
 <template>
-<h1>Test</h1>
+    <div class="container">
+        <app-photo :style='{gridColumn: "1/5", gridRow:"1/3"}'>
+                <img src="./photos/photo01.jpg" >
+        </app-photo>
+        <app-photo :style='{gridColumn: "5/7", gridRow:"1/2"}'>
+                <img src="./photos/photo02.jpg" >
+        </app-photo>
+        <app-photo :style='{gridColumn: "5/7", gridRow:"2/3"}'>
+                <img src="./photos/photo03.jpg" >
+        </app-photo>
+        <app-photo :style='{gridColumn: "1/3", gridRow:"3/4"}'>
+                <img src="./photos/photo04.jpg" >
+        </app-photo>
+        <app-photo :style='{gridColumn: "1/3", gridRow:"4/5"}'>
+                <img src="./photos/photo05.jpg" >
+        </app-photo>
+        <app-photo :style='{gridColumn: "3/7", gridRow:"3/5"}'>
+                <img src="./photos/photo06.jpg" >
+        </app-photo>
+        <app-photo :style='{gridColumn: "1/3", gridRow:"5/6"}'>
+                <img src="./photos/photo07.jpg" >
+        </app-photo>
+        <app-photo :style='{gridColumn: "3/5", gridRow:"5/6"}'>
+                <img src="./photos/photo09.jpg"  >
+        </app-photo>
+        <app-photo :style='{gridColumn: "5/7", gridRow:"5/6"}'>
+                <img src="./photos/photo08.jpg" >
+        </app-photo>
+         <app-photo :style='{gridColumn: "1/5", gridRow:"6/8"}'>
+                <img src="./photos/photo13.jpg" >
+        </app-photo>
+         <app-photo :style='{gridColumn: "5/7", gridRow:"6/7"}'>
+                <img src="./photos/photo11.jpg" >
+        </app-photo>
+         <app-photo :style='{gridColumn: "5/7", gridRow:"7/8"}'>
+                <img src="./photos/photo12.jpg" >
+        </app-photo>
+    </div>
 </template>
 
 <script>
+    import Photo from './components/Photo.vue';
+    
+    export default {
+        components: {
+            'appPhoto' : Photo
+        }
+    }
 </script>
 
 <style>
+    .container {
+        width:80%;
+        margin:auto;
+        display: grid;
+        grid-template-columns: 16% 16% 16% 16% 16% 16%;
+        grid-template-rows: 14% 14% 14% 14% 14% 14% 14%;
+        grid-gap:3px;
+    }
+    img {
+        width:100%;
+        border-radius:15px;
+    }
+
 </style>
---
+++ src/components/Photo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+    
+</template>
+
+<script>
+</script>
+
+<style>
+    img {
+         width:100%;
+        border-radius:8px;
+    }   
+</style>
